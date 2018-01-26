f650d60172cb828369c98798abe7e291364c740a
Pavel Antsypovich <pavelantsypovich@gmail.com>
2018-01-26 19:01:22 +0300

    aded mods & brightness, fixed bug with screen refresh

--- astrotracker.ino
+++ astrotracker.ino
@@ -1,39 +1,88 @@
 #include <LiquidCrystal.h>
-//ubrat drebezg
+//bag s neravnomernum izmeneniem skorosti (vinovat ekran, v serial vse pravilno)
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
+  bool key_flag_up = 0;
+  bool key_flag_down = 0;
+  int bright = 30;
   int  del = 100; //sensivity
-  int speed = 0;
+  int set_speed = 225; //speed
+  unsigned int speed = 0;
   int key = 0;
+  int mode = 0;
   unsigned long last_time;
   
   
 void setup() {
   lcd.begin(16,2);
-  
-}
+  pinMode(0, INPUT_PULLUP);
+  }
 
 void loop() {
- if (millis() - last_time > del) {
-  last_time = millis();
+  analogWrite (10, bright);
   key = analogRead(0);
-  if (key < 60) {
-   speed++;
-   }
-   else if (key < 200) {
-     //up
-   }
-   else if (key < 400){
-     //down
-   }
-   else if (key < 600){
-      speed--;
-    }
-   else if (key < 800){
-    
-    } 
-   lcd.setCursor(0,0);
-   lcd.print(speed);
+  lcd.setCursor(0,0);
+  lcd.print("Mode:");
+  lcd.setCursor(0,1);
+  lcd.print("Speed:");
+
+  if (key == 99 && key_flag_up == 0) {
+  mode++;
+  key_flag_up = 1;
+  lcd.clear();
   }
+  if (key == 255 && key_flag_down == 0) {
+  bright = bright +20;
+  key_flag_down = 1;
+  }
+  else if (key == 1023) {
+    key_flag_up = 0;
+    key_flag_down = 0;
+  }
+
+  if (mode == 3) {
+    mode = 0;
+  }
+   
+  if (mode == 0) {
+   manual();  
+  }
+  else if (mode == 1) {
+   full();  
+  }
+  else if (mode == 2) {
+    half();
+  }
+   lcd.setCursor(7,1);
+   lcd.print(speed);    
 }
+
+void manual() {
+  lcd.setCursor(6,0);
+  lcd.print("manual");
+ if (key == 0 && millis() - last_time > del) {
+   last_time = millis();
+   speed = speed + 10;
+   lcd.clear();
+   }
+ else if (key == 407 && millis() - last_time > del){
+  last_time = millis();
+  speed = speed - 10;
+  lcd.clear();
+    }    
+ }
+
+void full() {
+  lcd.setCursor(6,0);
+  lcd.print("full");  
+  speed = set_speed;
+}
+
+void half () {
+  lcd.setCursor(6,0);
+  lcd.print("half");
+  speed = set_speed/2;
+}
+
+
 
